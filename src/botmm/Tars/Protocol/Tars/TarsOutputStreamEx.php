a9e8f1017c566693ce6efba8725e400b58d90afe
xsilen <xsilen@163.com>
2017-02-17 15:23:32 +0800

    --

--- src/botmm/Tars/Protocol/Tars/TarsOutputStreamEx.php
+++ src/botmm/Tars/Protocol/Tars/TarsOutputStreamEx.php
@@ -12,34 +12,34 @@ class TarsOutputStreamEx
      * @param TarsOutputStream $jos
      */
     public static function write($e, int $tag, TarsOutputStream $jos) {
-        TarsStructInfo info = TarsHelper.getStructInfo(e.getClass());
-        if (info == null) {
-            throw new TarsEncodeException("the JavaBean[" + e.getClass().getSimpleName() + "] no annotation Struct");
-        }
-
-        jos.reserve(2);
-        jos.writeHead(TarsStructBase.STRUCT_BEGIN, tag);
-        List<TarsStrutPropertyInfo> propertysList = info.getPropertyList();
-        if (!CommonUtils.isEmptyCollection(propertysList)) {
-            for (TarsStrutPropertyInfo propertyInfo : propertysList) {
-                Object value = null;
-                try {
-                    value = BeanAccessor.getBeanValue(e, propertyInfo.getName());
-                } catch (Exception ex) {
-                    throw new TarsEncodeException(ex.getLocalizedMessage());
-                }
-
-                if (value == null && propertyInfo.isRequire()) {
-                    throw new TarsEncodeException(propertyInfo.getName() + " is require tag=" + propertyInfo.getOrder());
-                }
-
-                if (value != null) {
-                    jos.write(value, propertyInfo.getOrder());
-                }
-            }
-        }
-
-        jos.reserve(2);
-        jos.writeHead(TarsStructBase.STRUCT_END, 0);
+        //TarsStructInfo info = TarsHelper.getStructInfo(e.getClass());
+        //if (info == null) {
+        //    throw new TarsEncodeException("the JavaBean[" + e.getClass().getSimpleName() + "] no annotation Struct");
+        //}
+        //
+        //jos.reserve(2);
+        //jos.writeHead(TarsStructBase.STRUCT_BEGIN, tag);
+        //List<TarsStrutPropertyInfo> propertysList = info.getPropertyList();
+        //if (!CommonUtils.isEmptyCollection(propertysList)) {
+        //    for (TarsStrutPropertyInfo propertyInfo : propertysList) {
+        //        Object value = null;
+        //        try {
+        //            value = BeanAccessor.getBeanValue(e, propertyInfo.getName());
+        //        } catch (Exception ex) {
+        //            throw new TarsEncodeException(ex.getLocalizedMessage());
+        //        }
+        //
+        //        if (value == null && propertyInfo.isRequire()) {
+        //            throw new TarsEncodeException(propertyInfo.getName() + " is require tag=" + propertyInfo.getOrder());
+        //        }
+        //
+        //        if (value != null) {
+        //            jos.write(value, propertyInfo.getOrder());
+        //        }
+        //    }
+        //}
+        //
+        //jos.reserve(2);
+        //jos.writeHead(TarsStructBase.STRUCT_END, 0);
     }
 }
