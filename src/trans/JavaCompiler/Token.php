52b78326e275426c2bba020097a1faf961eb5c91
hugo <mldonkey@163.com>
2017-02-18 22:49:22 +0800

    Token翻译完毕

--- src/trans/JavaCompiler/Token.php
+++ src/trans/JavaCompiler/Token.php
@@ -12,55 +12,56 @@ namespace src\trans\JavaCompiler;
 class Token
 {
 
-constructor(
-public index: number, public type: TokenType, public numValue: number,
-public strValue: string) {}
+public  $index;
+public  $type;
+public $numValue;
+public $strValue;
 
-isCharacter(code: number): boolean {
-    return this.type == TokenType.Character && this.numValue == code;
+function isCharacter(int $code): boolean {
+    return $this->type == TokenType::Character && $this->numValue == $code;
 }
 
-  isNumber(): boolean { return this.type == TokenType.Number; }
+function isNumber(): boolean { return $this->type == TokenType::Number; }
 
-  isString(): boolean { return this.type == TokenType.String; }
+function isString(): boolean { return $this->type == TokenType::String; }
 
-  isOperator(operater: string): boolean {
-    return this.type == TokenType.Operator && this.strValue == operater;
+function  isOperator(string $operater): boolean {
+    return $this->type == TokenType::Operator && $this->strValue == $operater;
 }
 
-  isIdentifier(): boolean { return this.type == TokenType.Identifier; }
+function isIdentifier(): boolean { return $this->type == TokenType::Identifier; }
 
-  isKeyword(): boolean { return this.type == TokenType.Keyword; }
+function  isKeyword(): boolean { return $this->type == TokenType::Keyword; }
 
-  isKeywordLet(): boolean { return this.type == TokenType.Keyword && this.strValue == 'let'; }
+function  isKeywordLet(): boolean { return $this->type == TokenType::Keyword && $this->strValue == 'let'; }
 
-  isKeywordNull(): boolean { return this.type == TokenType.Keyword && this.strValue == 'null'; }
+function  isKeywordNull(): boolean { return $this->type == TokenType::Keyword && $this->strValue == 'null'; }
 
-  isKeywordUndefined(): boolean {
-    return this.type == TokenType.Keyword && this.strValue == 'undefined';
+function  isKeywordUndefined(): boolean {
+    return $this->type == TokenType::Keyword && $this->strValue == 'undefined';
 }
 
-  isKeywordTrue(): boolean { return this.type == TokenType.Keyword && this.strValue == 'true'; }
+function isKeywordTrue(): boolean { return $this->type == TokenType::Keyword && $this->strValue == 'true'; }
 
-  isKeywordFalse(): boolean { return this.type == TokenType.Keyword && this.strValue == 'false'; }
+function  isKeywordFalse(): boolean { return $this->type == TokenType::Keyword && $this->strValue == 'false'; }
 
-  isKeywordThis(): boolean { return this.type == TokenType.Keyword && this.strValue == 'this'; }
+function  isKeywordThis(): boolean { return $this->type == TokenType::Keyword && $this->strValue == 'this'; }
 
-  isError(): boolean { return this.type == TokenType.Error; }
+function  isError(): boolean { return $this->type == TokenType::Error; }
 
-  toNumber(): number { return this.type == TokenType.Number ? this.numValue : -1; }
+function toNumber(): number { return $this->type == TokenType::Number ? $this->numValue : -1; }
 
-  toString(): string {
-    switch (this.type) {
-        case TokenType.Character:
-        case TokenType.Identifier:
-        case TokenType.Keyword:
-        case TokenType.Operator:
-        case TokenType.String:
-        case TokenType.Error:
-            return this.strValue;
-        case TokenType.Number:
-            return this.numValue.toString();
+function  toString(): string {
+    switch ($this->type) {
+        case TokenType::Character:
+        case TokenType::Identifier:
+        case TokenType::Keyword:
+        case TokenType::Operator:
+        case TokenType::String:
+        case TokenType::Error:
+            return $this->strValue;
+        case TokenType::Number:
+            return $this->numValue.toString();
         default:
             return null;
     }
