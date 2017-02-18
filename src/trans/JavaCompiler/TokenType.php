5ff0376c58771e7f3d94cd8d32fea0b207c12a0c
xsilen <xsilen@163.com>
2017-02-18 22:13:07 +0800

    okentype

---
+++ src/trans/JavaCompiler/TokenType.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace src\trans\JavaCompiler;
+
+
+class TokenType
+{
+    public const Character  = 1;
+    public const Identifier = 2;
+    public const Keyword    = 3;
+    public const String     = 4;
+    public const Operator   = 5;
+    public const Number     = 6;
+    public const Erro       = 7;
+
+}
