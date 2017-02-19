0b23f775196c1209a14949c8144eb8bb5d110b5a
xsilen <xsilen@163.com>
2017-02-19 12:34:21 +0800

    add regex wrapper

--- src/trans/JavaCompiler/Wrapper/RegexWrapper.php
+++ src/trans/JavaCompiler/Wrapper/RegexWrapper.php
@@ -4,7 +4,27 @@
 namespace trans\JavaCompiler\Wrapper;
 
 
+use Spatie\Regex\Regex;
+
 class RegexWrapper
 {
+
+    /**
+     * @param $pattern
+     * @param $subject
+     * @return \Spatie\Regex\MatchResult
+     */
+    public static function match($pattern, $subject)
+    {
+        return Regex::match($pattern, $subject);
+    }
+
+    public static function matchAll($pattern, $subject) {
+        return Regex::matchAll($pattern, $subject);
+    }
+    
+    public static function replace($pattern, $replacement, $subject, $limit = -1) {
+        return Regex::replace($pattern, $replacement, $subject, $limit);
+    }
 
 }
